a88b98f50822d787b0eed2fc38712f6efc456751
LukasKepplinger <20162010@atn.ac.at>
2023-03-14 11:59:10 +0100

    cloudpb

--- src/lib/connections/pb.ts
+++ src/lib/connections/pb.ts
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase("http://109.205.182.22");
+const pb = new PocketBase("https://bulky-rose.pockethost.io");
 // const pb = new PocketBase("https://109.205.182.22");
 // const pb = new PocketBase("http://127.0.0.1:8090");
 export default pb;
